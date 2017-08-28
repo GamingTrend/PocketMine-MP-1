24f6ec11ad2cdaf839ed2bb986c18ff67ad98839
GamingTrend <rafeole123@gmail.com>
2017-08-28 18:18:11 +0200

    Create HomeScreenSubmite.py

---
+++ NonJailbreak/HomeScreenSubmite.py
@@ -0,0 +1,9 @@
+if a TAP_TO_HOMESCREEN
+    PocketMineMPIcon.png = 1
+PocketMineMP1.4.4v = 1
+API1.0.0 = 1
+<class 'load'>
+type(24)
+<class 'str'>
+if a OPEN_POCKETMINEMP
+    run POCKETMINEMP1.4.4v.php
